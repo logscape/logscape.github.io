ec73f7fb3a62a894beb9dc4ee0cb2cb1abab7df4
Zvenyika Gomo <zvenyika.gomo@excelian.com>
2014-06-12 16:48:41 +0100

    *update*

---
+++ js/kiss.js
@@ -0,0 +1,12 @@
+var _kmq = _kmq || [];
+var _kmk = _kmk || '53852b2c03f4377286788205db7c25bf27e00d58';
+function _kms(u){
+  setTimeout(function(){
+    var d = document, f = d.getElementsByTagName('script')[0],
+    s = d.createElement('script');
+    s.type = 'text/javascript'; s.async = true; s.src = u;
+    f.parentNode.insertBefore(s, f);
+  }, 1);
+}
+_kms('//i.kissmetrics.com/i.js');
+_kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
